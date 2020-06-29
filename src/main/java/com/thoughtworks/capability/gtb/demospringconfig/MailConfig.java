c75e53b5f3eb45658e773b6bd24065e241586acc
zhai xin <xin.zhai@thoughtworks.com>
2020-06-29 13:03:01 +0800

    finish p1s1

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,12 +2,18 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+    private List defaultRecipients;
+    private Map additionalHeaders;
+    private Credentials credentials;
 
     public String getHostname() {
         return hostname;
@@ -33,12 +39,40 @@ public class MailConfig {
         this.from = from;
     }
 
+    public List getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(List defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public Map getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAdditionalHeaders(Map additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
+    }
+
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
+                ", defaultRecipients=" + defaultRecipients +
+                ", additionalHeaders=" + additionalHeaders +
+                ", credentials=" + " username: " + credentials.getUsername()
+                + " password: " + credentials.getPassword() + " authMethod: " + credentials.getAuthMethod() +
                 '}';
     }
 }
