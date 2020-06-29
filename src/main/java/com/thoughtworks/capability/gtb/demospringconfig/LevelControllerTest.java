2a86a00df901fa3e0240e21ac7756eff151686ce
zhai xin <xin.zhai@thoughtworks.com>
2020-06-29 13:12:12 +0800

    finish p2s1

---
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,36 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest(classes = DemoSpringConfigApplication.class)
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@TestPropertySource(
+        properties = {
+                "levelNumber=2"
+        }
+)
+public class LevelControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldGetAdvanced() throws Exception {
+        String getLevelResult = mockMvc.perform(MockMvcRequestBuilders.get("/level"))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        Assert.assertEquals(getLevelResult, "advanced");
+    }
+}
